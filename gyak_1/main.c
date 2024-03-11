e700bacae44f1928ede4709ba2fd51a43ed036f2
Fehér Árpád <feherarpad92@gmail.com>
2024-03-11 17:45:20 +0100

    03.11. függvények

--- gyak_1/main.c
+++ gyak_1/main.c
@@ -16,6 +16,17 @@
 #define BEOLVAS(kor) \
 	printf("Kerem adja meg a szemely korat: "); \
 	scanf_s("%d", &kor);
+//3. feladat
+#define DEBUG(s) (printf("Hiba a(z) %d. sorban: ",__LINE__), printf("%s\n",s))
+//4. feladat
+#define HIBA_KEZELESE(feltetel, uzenet) \
+	if(feltetel) \
+	{ \
+		printf("Hiba: %s\n", uzenet); \
+		return 1; \
+	}
+
+#define TOMB_MERETE 5
 
 /******************************************************************************
 * Constants
@@ -35,7 +46,8 @@
 /******************************************************************************
 * Local Function Declarations
 ******************************************************************************/
-
+float atlag(int tomb[], int meret);
+void negyzet_es_kob(int szam, int* negyzet, int* kob);
 
 /******************************************************************************
 * Local Function Definitions
@@ -51,15 +63,52 @@
 int main(void)
 {
 	//1. feladat
-	int a;
-	a = 5;
-	int b = 6, c;
-	c = MAXIMUM(a, b);
-	printf("A nagyobb szam: %d\n", c);
+	//int a;
+	//a = 5;
+	//int b = 6, c;
+	//c = MAXIMUM(a, b);
+	//printf("A nagyobb szam: %d\n", c);
 	//2. feladat
-	unsigned int kor;
-	BEOLVAS(kor);
-	printf("A szemely kora: %u\n", kor);
+	//unsigned int kor;
+	//BEOLVAS(kor);
+	//printf("A szemely kora: %u\n", kor);
+	//3. feladat
+	//int d = 3;
+	//int t = 0;
+	
+	/*
+	if (t == 0)
+	{
+		DEBUG("nullaval valo osztas!!");
+	}
+	else
+	{
+		printf("%f", ((float)d / (float)t));
+	}
+	*/
+	//4. feladat
+	//HIBA_KEZELESE(t == 0, "Nullaval valo osztas!!");
+	//5. feladat
+	
+	//int tomb_merete = 5;
+
+	/*
+	int tomb[TOMB_MERETE];
+	for (int i = 0; i < TOMB_MERETE; i++)
+	{
+		scanf_s("%d", &tomb[i]);
+	}
+	float eredmeny = atlag(tomb, TOMB_MERETE);
+	printf("%f", eredmeny);
+	*/
+	//6. feladat
+	printf("Adjon meg egy szamot: ");
+	int szam = 0;
+	scanf_s("%d", &szam);
+	int negyzet = 0, kob = 0;
+	negyzet_es_kob(szam, &negyzet, &kob);
+	printf("\n%d szam negyzete: %d, kobe: %d", szam, negyzet, kob);
+	
 
 	/* Replace with your application code */
 	while (1)
@@ -68,13 +117,32 @@ int main(void)
 	}
 }
 
+/******************************************************************************
+* Function:         float atlag(int tomb[], int meret)
+* Description:      Átlag számítása
+* Input:			számokat tartalmazó tömb, tömb mérete
+* Output:			a számok átlaga
+* Notes:
+******************************************************************************/
+float atlag(int tomb[], int meret)
+{
+	int osszeg = 0;
+	for (int i = 0; i < meret; i++)
+	{
+		osszeg = osszeg + tomb[i];
+	}
+	return ((float)osszeg / (float)meret);
+}
 
 /******************************************************************************
-* Interrupt Routines
+* Function:         fvoid negyzet_es_kob(int szam, int* negyzet, int* kob)
+* Description:      Négzet és harmadik hatvány számítása
+* Input:			A szám és az eredmények memóriacíme
+* Output:			
+* Notes:
 ******************************************************************************/
-
-
-
-
-
-
+void negyzet_es_kob(int szam, int* negyzet, int* kob)
+{
+	*negyzet = szam * szam;
+	*kob = (*negyzet) * szam;
+}
