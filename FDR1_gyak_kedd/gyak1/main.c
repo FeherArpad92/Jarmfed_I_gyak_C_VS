7cd2738969cbc71cb3ca02e15bdb0b555f12db2f
Tamás WÁGNER <wagnertomi991025@gmail.com>
2026-03-30 16:06:17 +0200

    03.24.

--- FDR1_gyak_kedd/gyak1/main.c
+++ FDR1_gyak_kedd/gyak1/main.c
@@ -16,48 +16,19 @@
 
 #define BEOLVAS(kor) \
 	printf("Kerem adja meg a korat!"); \
-	scanf_s("%d",&kor)
-
-#define HIBA(msg) printf("[HIBA] fajl: %s, sor: %d, hibauzenet: %s",__FILE__, __LINE__, msg)
-
-#define CHECK_ERROR(feltetel, uzenet)\
-	do{\
-		if (feltetel)\
-		{\
-			printf("Vegzetes hiba: %s", uzenet);\
-			exit(1);\
-		}\
-	} while(0);
+	scanf_s("%d",&kor);
 
 int main(void)
 {
-	////1. feladat
-	//int x = 12;
-	//int y = 26;
+	int x = 12;
+	int y = 26;
 
-	//printf("A nagyobb szam: %d\n", MAXIMUM(x, y));
+	printf("A nagyobb szam: %d\n", MAXIMUM(x, y));
 
-	////2. feladat
-	//int szemely_kora = 0;
-	//BEOLVAS(szemely_kora);
+	int szemely_kora = 0;
+	BEOLVAS(szemely_kora);
 
-	//printf("A beolvasott kor: %d", szemely_kora);
-
-	//3. feladat
-	/*int homerseklet = 150;
-
-	if (homerseklet > 100)
-	{
-		HIBA("Tul magas homerseklet!");
-	}*/
-
-	//4.feladat 
-	int oszto = 0;
-	int osztando = 100;
-
-	CHECK_ERROR(oszto == 0, "Nullaval valo osztas nem lehetseges!");
-
-
+	printf("A beolvasott kor: %d", szemely_kora);
 	/* Replace with your application code */
 	while (1)
 	{
